298895096feb9aaa3fbd11352a5a8d8195daac9b
Sakshi Monga <saksh@microsoft.com>
2018-09-04 15:17:25 +0530

    [Update] QuestionBank updated-Required added

--- SurveyApp/SurveyApp/Models/IdentityModels.cs
+++ SurveyApp/SurveyApp/Models/IdentityModels.cs
@@ -12,9 +12,15 @@ namespace SurveyApp.Models
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
+
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+
+
             // Add custom user claims here
+            //Adding reference of Organization table in AspNetUser
+           // public virtual Organization Organizations { get; set; }
+            
             return userIdentity;
         }
     }
