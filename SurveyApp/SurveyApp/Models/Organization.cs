cd4a20cdc1b7b9b9e0f740fefdc038cdbdbe9f18
Sakshi Monga <saksh@microsoft.com>
2018-09-04 11:51:36 +0530

    [ Update] Added Foreign key in organization table

--- SurveyApp/SurveyApp/Models/Organization.cs
+++ SurveyApp/SurveyApp/Models/Organization.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -16,7 +18,12 @@ namespace SurveyApp.Models
         [Key]
         public int OrgId { get; set; }
         public string OrgName { get; set; }
-        public Nullable<int> AdminId { get; set; }
+       
+        
+        public string AdminId { get; set; }
+
+        [ForeignKey("AdminId")]
+        public virtual ApplicationUser ApplicationUserId { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Survey> Surveys { get; set; }
