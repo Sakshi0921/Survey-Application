fdf2b6c30e0c435904b28f36a186290df19804ea
Sakshi Monga <saksh@microsoft.com>
2018-09-27 20:26:20 +0530

    [Feature] AdminList

--- SurveyApp/SurveyApp/Controllers/HomeController.cs
+++ SurveyApp/SurveyApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SurveyApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,15 @@ namespace SurveyApp.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult AdminList()
+        {
+            var roleid = db.Roles.Where(r => r.Name == "Admin").Select(r=>r.Id).First();
+            // db.Users.Select(x=>x.Roles.)
+             ViewBag.AdminList =  db.Users.Where(x => (x.Roles.Any(y=>y.RoleId == roleid))).Select(u=> new UserViewModel { Email=u.Email,OrgName=u.Organizations.OrgName }).ToList();
+
+            return View();
+        }
         public ActionResult Index()
         {
 
---
+++ SurveyApp/SurveyApp/Models/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurveyApp.Models
+{
+    public class UserViewModel
+    {
+        public string OrgName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
